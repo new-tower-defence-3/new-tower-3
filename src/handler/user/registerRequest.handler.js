939ca6e8cf5defdda30fc2c627feb7da265a8d1d
P-lani <dndtkd2@naver.com>
2024-11-06 11:36:36 +0900

    ✨ update , response 부분 추가
    불필요한 콘솔도 삭제

--- src/handler/user/registerRequest.handler.js
+++ src/handler/user/registerRequest.handler.js
@@ -1,31 +1,40 @@
 ﻿import { createUser, findUserId } from '../../db/user/user.db.js';
+import CustomError from '../../utils/error/customError.js';
 import JoiUtils from '../../utils/joi.util.js';
 import bcrypt from 'bcrypt';
+import { createResponse } from '../../utils/response/createResponse.js';
+import { PacketType } from '../../constants/header.js';
 
 const registerRequestHandler = async ({ socket, payload, sequence }) => {
   try {
-    console.log('registerRequestHandler Called');
-    console.log(socket);
-    console.log(payload);
-    console.log(sequence);
-
     const { id, email, password } = await JoiUtils.validateSignUp(payload);
-
-    console.log(id);
-    console.log(password);
-    console.log(email);
 
     const checkExistId = await findUserId(id);
     if (checkExistId) {
-      console.log('이미 존재하는 ID');
-      return 1;
+      const responsePayload = {
+        success: false,
+        message: '이미 존재하는 ID',
+        failCode: 2,
+      };
+
+      const registerResponse = createResponse(PacketType.REGISTER_RESPONSE, responsePayload);
+      socket.write(registerResponse);
+
+      throw new CustomError(ErrorCodes.USER_NOT_FOUND, '이미 존재하는 ID');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const createUserId = await createUser(id, email, hashedPassword);
 
-    return 1;
+    const responsePayload = {
+      success: true,
+      message: 'Register Success',
+      failCode: 0,
+    };
+
+    const registerResponse = createResponse(PacketType.REGISTER_RESPONSE, responsePayload);
+    socket.write(registerResponse);
   } catch (e) {
     console.error(e);
   }
